036d25053c76af592d67604788a594d5dfc3564c
dsharma <dsharma@192.168.0.104>
2017-06-02 15:58:08 +0530

    Fix layout of dogs datatable

--- src/main/resources/ktrack/ui/js/doglist.js
+++ src/main/resources/ktrack/ui/js/doglist.js
@@ -5,12 +5,14 @@ function renderThumbNail( data, type, row, meta ) {
 		window.dogList.snapshotUrl = '${SNAPSHOTURL}';
 		window.dogList.previewfileKey = '${PREVIEWKEY}';
 		window.dogList.renderSnapshotThumbnail = function(data, type, row, meta) {
-			var html = '<div class=\"row\">';
+			var html = '';
+			var colLength = 12 / $(data).length; 
 			$.each(data, function(index, value) {
-				html += '<div class="thumbnail col-xs-4"><a  class="thumbNailLink" href="#" id="' + value + '" >';			
+				var cls = "thumbnail col-xs-"  + colLength;
+				html += '<div style="margin-bottom: 0" class="' + cls + '" ><a  class="thumbNailLink" href="#" id="' + value + '" >';			
 				html += '<img src="' + dogList.snapshotUrl +'?imageId=' + value + '" /></a></div>';
 			});
-			html += '</div>';
+		
 			return html;
 		};
 		
